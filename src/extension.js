e6063d7cc028ff9e82911b8db8dd491a1b770f71
pencilq <hqysmail@gmail.com>
2026-05-22 21:25:03 +0800

    cancel  zh filed data

--- src/extension.js
+++ src/extension.js
@@ -543,7 +543,7 @@ let _fieldData = null;
 
 function getFieldData() {
     if (!_fieldData) {
-        const dataPath = path.join(__dirname, '..', 'keywords', 'field_data_zh.json');
+        const dataPath = path.join(__dirname, '..', 'keywords', 'field_data.json');
         try {
             _fieldData = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
         } catch {
