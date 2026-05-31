98a853860be4dd7683f0e585965f73784bd1c201
pencilq <hqysmail@gmail.com>
2026-05-31 23:01:09 +0800

    ui: Move include validation icons from inline text to gutter icons

--- src/extension.js
+++ src/extension.js
@@ -2265,23 +2265,19 @@ function activate(context) {
     );
 
     // Decorations: green for resolved paths, yellow for missing ones
+    const checkmarkSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path fill="%2389d185" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path></svg>`;
     const resolvedDecoration = vscode.window.createTextEditorDecorationType({
         color: new vscode.ThemeColor('textLink.foreground'),
-        after: {
-            contentText: ' ✓',
-            color: new vscode.ThemeColor('gitDecoration.untrackedResourceForeground'),
-            margin: '0 0 0 5px'
-        }
+        gutterIconPath: vscode.Uri.parse(`data:image/svg+xml;utf8,${checkmarkSvg}`),
+        gutterIconSize: 'contain',
     });
+    
+    const warningSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path fill="%23cca700" d="M8.22 1.754a.25.25 0 00-.44 0L1.698 13.132a.25.25 0 00.22.368h12.164a.25.25 0 00.22-.368L8.22 1.754zm-1.763-.707c.659-1.234 2.427-1.234 3.086 0l6.082 11.378A1.75 1.75 0 0114.082 15H1.918a1.75 1.75 0 01-1.543-2.575L6.457 1.047zM9 11a1 1 0 11-2 0 1 1 0 012 0zm-.25-5.25a.75.75 0 00-1.5 0v2.5a.75.75 0 001.5 0v-2.5z"></path></svg>`;
     const missingDecoration = vscode.window.createTextEditorDecorationType({
         color: new vscode.ThemeColor('list.warningForeground'),
         fontStyle: 'italic',
-        after: {
-            contentText: ' ⚠',
-            color: new vscode.ThemeColor('list.warningForeground'),
-            margin: '0 0 0 5px',
-            fontStyle: 'normal'
-        }
+        gutterIconPath: vscode.Uri.parse(`data:image/svg+xml;utf8,${warningSvg}`),
+        gutterIconSize: 'contain',
     });
     const keywordDecoration = vscode.window.createTextEditorDecorationType({
         fontWeight: 'bold'
